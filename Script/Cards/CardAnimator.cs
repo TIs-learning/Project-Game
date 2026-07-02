1c83d680ab7a677f9d851a7f579f06707d940fd8
VarnT <caniansreal@gmail.com>
2026-07-02 20:03:19 +0700

    Update CardAnimator.cs

--- Script/Cards/CardAnimator.cs
+++ Script/Cards/CardAnimator.cs
@@ -1,33 +1,54 @@
+// Import Library
 using System.Collections;
 using UnityEngine;
 
+// Definisi Class (Inheritance)
 public class CardAnimator : MonoBehaviour
-{
+{    
+    // Method MoveTo()
     public IEnumerator MoveTo(Transform target, float duration)
     {
+        // Menyimpan Data Awal
         Vector3 start = transform.position;
         Vector3 end = target.position;
+
+        // Menentukan Perubahan Skala
         Vector3 startScale = transform.localScale;
         Vector3 endScale = startScale * 0.75f;
+
+        // Menentukan Rotasi
         Quaternion startRot = transform.rotation;
         Quaternion endRot = Quaternion.Euler(0,0,8);
 
+        // Inisialisasi Timer
         float timer = 0f;
 
+        // Perulangan Animasi
         while (timer < duration)
-        {
+        {    
+            // Menghitung Progress
             float t = timer / duration;
-            t = t * t * (3f - 2f * t);   // SmoothStep
 
+            // SmoothStep
+            t = t * t * (3f - 2f * t);   
+
+            // Memindahkan Kartu
             transform.position = Vector3.Lerp(start, end, t);
+
+            // Mengubah Ukuran
             transform.localScale = Vector3.Lerp(startScale, endScale, t);
+
+            // Mengubah Rotasi
             transform.rotation = Quaternion.Lerp(startRot, endRot, t);
 
+            // Menambah Timer
             timer += Time.deltaTime;
 
+            // Menunggu Frame Berikutnya
             yield return null;
         }
 
+        // Menyelesaikan Animasi
         transform.position = end;
     }
 }
