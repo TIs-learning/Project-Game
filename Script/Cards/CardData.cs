f29165e0508b3f5769a7fd97832dc8c1207aa0f0
VarnT <caniansreal@gmail.com>
2026-07-03 04:53:50 +0700

    Update CardData.cs

--- Script/Cards/CardData.cs
+++ Script/Cards/CardData.cs
@@ -1,22 +1,32 @@
+// Import Library
 using UnityEngine;
 
+// Membuat Asset Menu baru
 [CreateAssetMenu(fileName = "New Card", menuName = "Card Game/Card")]
-public class CardData : ScriptableObject{
-    [Header("Basic Info")]
-    public string cardName;
 
+// Deklarasi kelas
+public class CardData : ScriptableObject{
+
+    // Basic Info
+    [Header("Basic Info")]
+    // Nama kartu
+    public string cardName;
+    // Deskripsi
     [TextArea]
     public string description;
+    // Menyimpan gambar kartu
     public Sprite artwork;
 
+    // Menyimpan judul kedua pada inspector
     [Header("Card Properties")]
+    // Menentukan elemen kartu.
     public CardElement element;
-
+    // Menentukan jenis kartu.
     public CardKind kind;
-
+    // Menentukan efek yang dimiliki kartu.
     public EffectType effect;
-
+    // Besarnya efek kartu.
     public int value;
-
+    // Menentukan apakah kartu memiliki Element Symbol.
     public bool hasSymbol;
 }
